4fa3524fc53f972edc163d4560e5ec26f5d9d3e0
胡东山 <mj19910923@sina.com>
2022-11-30 17:18:21 +0800

    SmartDoc 测试

---
+++ spring-boot-starter-watch-cat-example/src/main/java/com/cat/example/web/ResultTestController.java
@@ -0,0 +1,33 @@
+package com.cat.example.web;
+
+import com.cat.example.ping.ResultPing;
+import com.cat.result.ResultData;
+import com.cat.watchcat.log.annotation.LogCat;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 请求详情对象封装测试
+ * @author hudongshan
+ * @version 2021/11/22
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "result", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ResultTestController {
+
+    @LogCat(actionGroup = "result", action = "case1", enableEvent = false)
+    @PostMapping("result/{a}")
+    public ResultData<ResultPing> strToLocalDate(@PathVariable String a) {
+
+        log.info("a={}",a);
+
+        return ResultData.ok().data(ResultPing.builder()
+                .name("zhangsan")
+                .phone("15188888888")
+                .build());
+    }
+
+
+}
