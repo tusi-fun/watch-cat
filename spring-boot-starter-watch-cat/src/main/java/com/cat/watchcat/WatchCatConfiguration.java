7a61d16f796f2b696336fe351ca99380c297adec
胡东山 <mj19910923@sina.com>
2022-12-05 16:07:50 +0800

    增加一些注释

--- spring-boot-starter-watch-cat/src/main/java/com/cat/watchcat/WatchCatConfiguration.java
+++ spring-boot-starter-watch-cat/src/main/java/com/cat/watchcat/WatchCatConfiguration.java
@@ -27,7 +27,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author hudongshan
  * @version 2021/10/14
  *
- * @ConditionalOnMissingBean，它是修饰bean的一个注解，主要实现的是，当bean被注册之后，再注册相同类型的bean，就不会成功，它会保证你的bean只有一个，即你的实例只有一个，当你注册多个相同的bean时，会出现异常
+ * @ConditionalOnMissingBean，修饰bean的注解，主要实现当bean被注册之后，再注册相同类型的bean，就不会成功，
+ * 它会保证你的bean只有一个，即你的实例只有一个，当你注册多个相同的bean时，会出现异常
  */
 @Configuration
 @EnableConfigurationProperties({
