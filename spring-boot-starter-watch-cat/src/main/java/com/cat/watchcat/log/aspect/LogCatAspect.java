43f5944e7e3b7d1535eccaaf8b77cf810655ac72
胡东山 <mj19910923@sina.com>
2023-03-13 11:14:02 +0800

    根据 ChatGPT 建议，优化了部分代码

--- spring-boot-starter-watch-cat/src/main/java/com/cat/watchcat/log/aspect/LogCatAspect.java
+++ spring-boot-starter-watch-cat/src/main/java/com/cat/watchcat/log/aspect/LogCatAspect.java
@@ -28,7 +28,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.util.*;
 
 /**
