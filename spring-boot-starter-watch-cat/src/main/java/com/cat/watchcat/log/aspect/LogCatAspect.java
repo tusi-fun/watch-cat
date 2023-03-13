a12df3d1c1cd54b4fa57c8e954fa854daf39d6ba
胡东山 <mj19910923@sina.com>
2023-03-13 11:06:19 +0800

    根据 ChatGPT 建议，优化了部分代码

--- spring-boot-starter-watch-cat/src/main/java/com/cat/watchcat/log/aspect/LogCatAspect.java
+++ spring-boot-starter-watch-cat/src/main/java/com/cat/watchcat/log/aspect/LogCatAspect.java
@@ -248,7 +248,7 @@ public class LogCatAspect {
         ExpressionParser parser = new SpelExpressionParser();
 
         // SPEL上下文 使用 StandardEvaluationContext 有注入的隐患， SimpleEvaluationContext 比较安全
-        // SimpleEvaluationContext context = new SimpleEvaluationContext.Builder().build();
+        // 使用安全的上下文：SPEL提供了两种上下文对象，即StandardEvaluationContext和SimpleEvaluationContext。其中，StandardEvaluationContext是支持注入的，而SimpleEvaluationContext则是不支持注入的。因此，在使用SPEL时应该使用SimpleEvaluationContext对象，以避免注入攻击
         EvaluationContext context = SimpleEvaluationContext.forReadOnlyDataBinding().build();
 
         // 把方法参数放入SPEL上下文中
