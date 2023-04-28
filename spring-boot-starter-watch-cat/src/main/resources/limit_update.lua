6dd435510b4180dcc7095681c4b28a62bfcf0709
胡东山 <mj19910923@sina.com>
2023-04-28 14:10:16 +0800

    starter 根据官方命名规范重命名

--- spring-boot-starter-watch-cat/src/main/resources/limit_update.lua
+++
@@ -1,9 +0,0 @@
-local key = KEYS[1]
-local exprie = ARGV[1]
-
--- 更新流控计数
-local currentValue = tonumber(redis.pcall("INCRBY", key, 1))
-if currentValue == 1 then
-    redis.pcall("EXPIRE", key, exprie)
-end
-return currentValue
