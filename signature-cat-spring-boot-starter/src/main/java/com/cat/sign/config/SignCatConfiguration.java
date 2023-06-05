dae13ab715cbce079537f119a94e252725a7f145
胡东山 <mj19910923@sina.com>
2023-06-05 17:56:26 +0800

    log、limit 拆分为独立的 starter

---
+++ signature-cat-spring-boot-starter/src/main/java/com/cat/sign/config/SignCatConfiguration.java
@@ -0,0 +1,89 @@
+package com.cat.sign.config;
+
+import com.cat.sign.aspect.SignCatAspect;
+import com.cat.sign.service.SignCommonService;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * watch-cat 配置
+ * @author hudongshan
+ * @version 2021/10/14
+ *
+ * - @ConditionalOnMissingBean
+ * 修饰bean的注解，主要实现当bean被注册之后，再注册相同类型的bean，就不会成功，
+ * 它会保证你的bean只有一个，即你的实例只有一个，当你注册多个相同的bean时，会出现异常
+ */
+@Slf4j
+@Configuration
+@EnableConfigurationProperties({
+        SignProperties.class
+})
+public class SignCatConfiguration {
+
+    @ConditionalOnWebApplication
+    @ConditionalOnProperty(prefix = "watchcat.sign", name = "enabled", havingValue = "true")
+    @Bean
+    public SignCatAspect signCatAspect() {
+        log.info("Initializing {} > {}", this.getClass().getSimpleName(), "signCatAspect");
+        return new SignCatAspect();
+    }
+
+    @ConditionalOnWebApplication
+    @ConditionalOnProperty(prefix = "watchcat.sign", name = "enabled", havingValue = "true")
+    @Bean
+    public SignCommonService cacheService(RedisTemplate redisTemplate) {
+        log.info("Initializing {} > {}", this.getClass().getSimpleName(), "cacheService");
+        return new SignCommonService(redisTemplate);
+    }
+
+    /**
+     * @ConditionalOnMissingBean 和 @ConditionalOnProperty 同时使用，为 AND 关系
+     * @param factory
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "watchcat.sign", name = "enabled", havingValue = "true")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+
+        log.info("Initializing {} > {}", this.getClass().getSimpleName(), "redisTemplate");
+
+        ObjectMapper om = new ObjectMapper();
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+
+        RedisTemplate<String, Object> wcRedisTemplate = new RedisTemplate<>();
+        wcRedisTemplate.setConnectionFactory(factory);
+        wcRedisTemplate.setKeySerializer(stringSerializer);
+        wcRedisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        wcRedisTemplate.setHashKeySerializer(stringSerializer);
+        wcRedisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        wcRedisTemplate.afterPropertiesSet();
+
+        return wcRedisTemplate;
+    }
+
+}
---
+++ signature-cat-spring-boot-starter/src/main/java/com/cat/sign/util/ApiSignUtils4Asymmetric.java
@@ -0,0 +1,218 @@
+package com.cat.sign.util;
+
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.HexUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.asymmetric.Sign;
+import cn.hutool.crypto.asymmetric.SignAlgorithm;
+import com.cat.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+/**
+ * 签名工具（适用于非对称算法rsa、sm2等）
+ * 1、参数为空，不参与签名
+ * 2、使用sm2算法
+ *
+ * @author hudongshan
+ * @version 20200803
+ */
+@Slf4j
+public class ApiSignUtils4Asymmetric {
+
+    public static final String
+            PATH_KEY = "path",
+            CONTENT_MD5_KEY = "content-md5",
+            METHOD_KEY = "method",
+            TIMESTAMP_KEY = "timestamp",
+            NONCE_KEY = "nonce",
+            SIGN_KEY = "sign";
+
+    /**
+     * 对请求内容签名
+     * @param requestParams 请求参数（form + path 中的参数）
+     * @param method 请求方法
+     * @param path
+     * @return
+     */
+    public Map<String, String> sign(String publicKey,
+                                    String privateKey,
+                                    SignAlgorithm signAlgorithm,
+                                    String method,
+                                    String path,
+                                    Map<String, String> requestParams) {
+
+        log.info("计算签名: method = " + method + ", path = " + path + ", requestParams = " + requestParams);
+
+        // 请求签名后的返回内容
+        Map<String,String> signResult = new HashMap();
+        signResult.put(TIMESTAMP_KEY, String.valueOf(System.currentTimeMillis()/1000L));
+        signResult.put(NONCE_KEY, RandomUtil.randomString(16));
+
+        // 构建签名体
+        Map<String,String> signBody = requestParams!=null?new HashMap(requestParams):new HashMap();
+        signBody.putAll(signResult);
+        signBody.put(METHOD_KEY,method);
+        signBody.put(PATH_KEY,path);
+        signResult.put(SIGN_KEY, genSign(publicKey,privateKey,signAlgorithm,signBody));
+
+        return signResult;
+    }
+
+    /**
+     * 校验签名
+     * @param requestParams 请求参数
+     * @return
+     */
+    public Boolean verify(String publicKey,
+                          SignAlgorithm signAlgorithm,
+                          String sign,
+                          String nonce,
+                          String timestamp,
+                          Map<String, String> requestParams) {
+
+        log.info("验证签名: sign = " + sign + ", nonce = " + nonce + ", timestamp = " + timestamp + ", requestParams = " + requestParams);
+
+        // 验证公钥提供者
+//        SymmetricSignProvider symmetricSignProvider = platformSymmetricProperties.getSymmetric().get(pulicKeyProvider);
+
+//        Assert.notNull(symmetricSignProvider,"验证签名: 签名提供者（"+pulicKeyProvider+"）不存在");
+//        Assert.hasLength(symmetricSignProvider.getAlgorithm(),"验证签名: 签名提供者（"+pulicKeyProvider+"）参数Algorithm未配置");
+//        Assert.hasLength(symmetricSignProvider.getPublicKey(),"验证签名: 签名提供者（"+pulicKeyProvider+"）参数PublicKey未配置");
+        Assert.isTrue(StringUtils.hasText(sign),"验证签名: "+SIGN_KEY+"不合法");
+        Assert.isTrue(StringUtils.hasText(nonce),"验证签名: "+NONCE_KEY+"不合法");
+
+//        Assert.isTrue(SignUtils.verifyTimestamp(timestamp,symmetricSignProvider.getTolerant()), "验证签名: "+ApiSignKeyEnum.TIMESTAMP_KEY+"不合法");
+//        // 验证签名值是否被使用过，存在则抛异常
+//        Assert.isTrue(cacheService.cacheSign(sign,symmetricSignProvider.getTolerant()),"验证签名: sign 已经使用过");
+
+        Map<String, String> signBody = new HashMap(requestParams);
+        signBody.put(NONCE_KEY,nonce);
+        signBody.put(TIMESTAMP_KEY,timestamp);
+
+        // 构建签名体
+        String signStr = buildSignBody(signBody);
+
+//        SignAlgorithm signAlgorithm = null;
+//
+//        try {
+//
+//            signAlgorithm = SignAlgorithm.valueOf(symmetricSignProvider.getAlgorithm());
+//
+//        } catch (IllegalArgumentException e) {
+//            log.error("验证签名: 签名算法无效 = {}",symmetricSignProvider.getAlgorithm());
+//        }
+//        Assert.notNull(signAlgorithm,"验证签名: 签名算法无效");
+
+        Sign rsaSign = SecureUtil.sign(signAlgorithm,null,publicKey);
+
+        try {
+
+            return rsaSign.verify(signStr.getBytes(), HexUtil.decodeHex(sign));
+
+        } catch (Exception e) {
+
+            log.error("验证签名: 执行值验证异常 = {}",e.getMessage());
+
+            throw new IllegalArgumentException("验证签名: sign格式有误，必须为16进制字符");
+        }
+
+    }
+
+    /**
+     * 生成 Sign 值 (排序、去空值、计算签名)
+     * @param signBody （包含业务参数、nonce、timestamp、accept、content-type）
+     */
+    private String genSign(String publicKey,String privateKey,SignAlgorithm signAlgorithm,Map<String,String> signBody) {
+
+        log.info("计算签名: signBody = " + signBody);
+
+        String signStr = buildSignBody(signBody);
+
+        // 生成 Sign 值
+//        SymmetricSignProvider symmetricSignProvider = platformSymmetricProperties.getSymmetric().get(pulicKeyProvider);
+
+//        Assert.notNull(symmetricSignProvider,"计算签名: 签名提供者（"+pulicKeyProvider+"）不存在");
+//        Assert.hasLength(symmetricSignProvider.getAlgorithm(),"计算签名: 签名提供者（"+pulicKeyProvider+"）参数Algorithm未配置");
+//        Assert.hasLength(symmetricSignProvider.getPublicKey(),"计算签名: 签名提供者（"+pulicKeyProvider+"）参数PublicKey未配置");
+//        Assert.hasLength(symmetricSignProvider.getPrivateKey(),"计算签名: 签名提供者（"+pulicKeyProvider+"）参数PrivateKey未配置");
+
+//        SignAlgorithm signAlgorithm = null;
+//
+//        try {
+//
+//            signAlgorithm = SignAlgorithm.valueOf(symmetricSignProvider.getAlgorithm());
+//
+//        } catch (IllegalArgumentException e) {
+//
+//            log.error("计算签名：签名算法无效 = {}",symmetricSignProvider.getAlgorithm());
+//        }
+//
+//        Assert.notNull(signAlgorithm,"计算签名: 签名算法无效");
+
+        Sign sign = SecureUtil.sign(signAlgorithm, privateKey, publicKey);
+
+        return HexUtil.encodeHexStr(sign.sign(signStr.getBytes()));
+    }
+
+    private String buildSignBody(Map<String,String> signBody) {
+
+        // 移除签名体中已有的 sign 参数（如果存在的话）
+        signBody.remove(SIGN_KEY);
+
+        // 获取 signBody 中的 key 集合
+        List<String> keyList = new ArrayList(signBody.keySet());
+
+        // 排序
+        Collections.sort(keyList);
+
+        // 构建签名内容
+        StringBuilder sb = new StringBuilder();
+
+        for (String key:keyList) {
+            String value = signBody.get(key);
+            if(StringUtils.hasText(value)) {
+                sb.append(key).append(value);
+            }
+        }
+
+        log.info("构建签名体：签名原文 = {}",sb);
+
+        return sb.toString();
+
+    }
+
+
+//    public static void main(String[] args) {
+//
+//        // 生成密钥对
+//        KeyPair pair = SecureUtil.generateKeyPair("RSA");
+//
+//        String privateKey = Base64.encode(pair.getPrivate().getEncoded());
+//        System.out.println("privateKey = " + privateKey);
+//
+//        String publicKey = Base64.encode(pair.getPublic().getEncoded());
+//        System.out.println("publicKey = " + publicKey);
+//
+//        byte[] data = "我是一段测试字符串".getBytes();
+//        Sign sign = SecureUtil.sign(SignAlgorithm.SHA256withRSA,privateKey,publicKey);
+//
+//        PrivateKey privateKey = SecureUtil.generatePrivateKey("RSA",Base64.decode("MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCPPCZWczCLCgAcPdGQTI5lgxS/47KSd8OZuNzcZnBR04TkWgIyfTNLesWXswznEKlV/b4fba3oko07ALE+UT7CvDjQTxBnmOhGg96EX+F7HGfSAnxlvqCN8zsQ+AmYbsMoKhFznhj0eIQPKOLaSCcoQGh9UhOOOP91jkfmgc5O8BuSBYScaxf/P30rhjoUD3eJg1Rdz+sRYt7w3BMcE4LQBZ3ZmlWBogKFbS2jvYz7bIjpui/Qai/YWd1/MxlFQV0RY10d0yrK2z87VHMnPZj8WxClbv/Jfawc1+4PtbEhmNQnVuNMxFg/kRM++q6zUN+x1Us0cCtv7JKBNCKqmornAgMBAAECggEAEn30gSK7+Kdg3kSI4rVPiQGEU0XFTy2RHi6ErRu4kofZV1XbhMMvNSZzjiM6njZDdmuc+RfC5QzOmPh86Yu5q5B7UUFz37wY+Mmbl8F9LdS7/pv9jXEv70Ogs48iRgC+x1evCxs1I5LPkdl4eC/qhkLSeqN3DNyE9ptY4wSNKxmY4AeJhzcEZITwZ6/5B8jAQDT3+NjZ7N+qYOtDL1BAURjziNel42j5XYcHtqTDwFfw4lNFxRl7p8JjRyMJN05VziokuprxJxT70rsc/Cj1GiG6ajfEMcd+hf7F/4kj2KKWOyWLH6RdAC/vJB8PDO2L3v3vJyXC3rocknzcoBAMWQKBgQDW1gnIA+x7vmGtkZlNTI7+rr67PrNJIkQFXX9NbCXFEHgRCD3ASrCaJ5Uyj/zpICYMiYPTQt9RI2whpPhiV4XiLGQYqZWrUtq6Npz0K5vHV2/CUsfDADCNqKf8d4pOF+aw5Iz03mNqVfMqiRpctw/D9PQvr3hmUFa6U4TjT4eivQKBgQCqrf4MsSrXkfGZ2Ml5Dj+CqK/WNwUR1LBQvPnUlhOSrshhOXf5Hqa2FIzv+5+mCB/upX6zria6e4Aky9kYtQuYh96IemwpJXVP/MqgcDZHjQ1MNOAbbVx65x9kxX1PUUUBtuuCBEEj/pYnR3bfCpusC33U5fkCjzt1lbFz6d0wcwKBgQCf3n0+43SajQzg/aUn+Z27dkwmyLzf4mjd2Tq/YYglwmCmxAw3yRzsSiJjkvSwgqTt1XNMxcmq9oIj80CG5fySV1hRZfkjma98vOFFbiIpUC6xW6qduMlo0SXY4RgBjxWzcxBVtBwk6Drg30/HyM4pk2IXDiu5b3VqiHYuNWOIvQKBgQCiuzOuvDihwid9TMGDJv+MvvKvplOyFXxp7lOwycotn3GzqQxnPD8s+VkK8njb34E6kxXDkPah9GBQNP9vXCaLLngQ3dBERs+cDOTQpUxLWgPktcE6xUFYeQZomRcJye2mXdYbZ50ICZL2Ll/69m1IUsiNdRq+wvuo96lyX/ahWwKBgQCW4FCwbZG9mcUfzfXTvx/FdMI++QMZOrtRhtJL6K6Sh8OmkW0VPnGKWrwOiblN4yKjdHQ7eG5t8ielTzJebk6TpxlXoLs4jz6YEoW23sscuA+YlErBoX5elDCqhl/DOSaP4EUbDJUQwmjNb/ajDbhMQxqbSSKF9WD/RIsKlfpavQ=="));
+//
+//        System.out.println(privateKey);
+//
+//        //签名
+//        byte[] signed = sign.sign(data);
+//
+//        String signStr = HexUtil.encodeHexStr(signed);
+//        System.out.println(signStr);
+//
+//        //验证签名
+//        boolean verify = sign.verify(data, HexUtil.decodeHex(signStr));
+//
+//        System.out.println(verify);
+//    }
+
+}
